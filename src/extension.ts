1d0c02a86c7d2f365b80be8c476c3de66f01c006
SONGHEE <pasong0511@gmail.com>
2025-01-06 01:15:12 +0900

    feat : 영구저장 데이터 리셋

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,26 @@ import { Group } from "./node/Group";
 import { Tab } from "./node/Tab";
 
 export function activate(context: vscode.ExtensionContext) {
-    //context.globalState 지우기 기능
-    //clearGlobalState(context);
+    /** 글로벌에 있는 context.globalState 데이터 깔끔하게 초기화*/
+
+    //TODO : provider로 빼기
+    context.subscriptions.push(
+        vscode.commands.registerCommand("global.state.reset", () => {
+            // vscode.window.showInformationMessage(
+            //     "Global State Reset Command Executed"
+            // );
+
+            clearGlobalState(context);
+        })
+    );
+
+    // option1 명령 핸들러
+    context.subscriptions.push(
+        vscode.commands.registerCommand("option1", () => {
+            vscode.window.showInformationMessage("Option 1 Command Executed");
+        })
+    );
+
     context.subscriptions.push(new TabView(context));
 }
 
