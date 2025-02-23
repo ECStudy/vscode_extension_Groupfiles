d264e3b3efd9ae56e0e0c7e26be5efd9ef54b408
SONGHEE <pasong0511@gmail.com>
2025-02-23 15:00:02 +0900

    fix : 그룹 아이콘 모양 변경

--- src/view/color.ts
+++ src/view/color.ts
@@ -1,9 +1,32 @@
+// export const colorPalette = [
+//     { label: "red", description: "Red", svg: "🔴" },
+//     { label: "orange", description: "Orange", svg: "🟠" },
+//     { label: "yellow", description: "Yellow", svg: "🟡" },
+//     { label: "green", description: "Green", svg: "🟢" },
+//     { label: "blue", description: "Blue", svg: "🔵" },
+//     { label: "purple", description: "Purple", svg: "🟣" },
+//     { label: "black", description: "Black", svg: "⚫" },
+// ];
+
+// export const colorPalette = [
+//     { label: "Red", description: "Red", svg: "🟥" },
+//     { label: "Orange", description: "Orange", svg: "🟧" },
+//     { label: "Yellow", description: "Yellow", svg: "🟨" },
+//     { label: "Green", description: "Green", svg: "🟩" },
+//     { label: "Blue", description: "Blue", svg: "🟦" },
+//     { label: "Purple", description: "Purple", svg: "🟪" },
+//     { label: "Brown", description: "Brown", svg: "🟫" },
+//     { label: "Black", description: "Black", svg: "⬛" },
+//     { label: "White", description: "White", svg: "⬜" },
+// ];
 export const colorPalette = [
-    { label: "red", description: "Red", svg: "🔴" },
-    { label: "orange", description: "Orange", svg: "🟠" },
-    { label: "yellow", description: "Yellow", svg: "🟡" },
-    { label: "green", description: "Green", svg: "🟢" },
-    { label: "blue", description: "Blue", svg: "🔵" },
-    { label: "purple", description: "Purple", svg: "🟣" },
-    { label: "black", description: "Black", svg: "⚫" },
+    { label: "Red", description: "Red", svg: "🔴" },
+    { label: "Orange", description: "Orange", svg: "🟠" },
+    { label: "Yellow", description: "Yellow", svg: "🟡" },
+    { label: "Green", description: "Green", svg: "🟢" },
+    { label: "Blue", description: "Blue", svg: "🔵" },
+    { label: "Purple", description: "Purple", svg: "🟣" },
+    { label: "Brown", description: "Brown", svg: "🟤" },
+    { label: "Black", description: "Black", svg: "⚫" },
+    { label: "White", description: "White", svg: "⚪" },
 ];
