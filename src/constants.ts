068466d3bd8f6f6d32aa819f7c694e6e7c0c1b72
SONGHEE <pasong0511@gmail.com>
2025-03-07 22:38:25 +0900

    refactor : const 파일, assets 파일 생성

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+//constants.ts
+export const colorPalette = [
+    { label: "red", icon: "🔴" },
+    { label: "orange", icon: "🟠" },
+    { label: "yellow", icon: "🟡" },
+    { label: "green", icon: "🟢" },
+    { label: "blue", icon: "🔵" },
+    { label: "purple", icon: "🟣" },
+    { label: "brown", icon: "🟤" },
+    { label: "black", icon: "⚫" },
+    { label: "white", icon: "⚪" },
+];
